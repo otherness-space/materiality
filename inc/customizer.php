c573ba9d7fcc31b957788c7f0590495a24f3043b
otherness-space <the@otherness.space>
2015-12-07 14:42:04 -0500

    First commit

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Materiality Theme Customizer.
+ *
+ * @package Materiality
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function materiality_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'materiality_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function materiality_customize_preview_js() {
+	wp_enqueue_script( 'materiality_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'materiality_customize_preview_js' );
